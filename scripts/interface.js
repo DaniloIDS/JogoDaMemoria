3e1794e05f30bc305121d05c5f72e7c93d7294e1
DaniloIDS <danilo.ids@outlook.com>
2022-02-10 14:06:49 -0300

    Insert cards by JS and flip

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,25 +1,37 @@
-let BACK = "back_card"
-let FRONT = "front_card"
+const BACK = "back_card"
+const FRONT = "front_card"
+const CARD = "card"
+const ICON = "icon"
+
 
 let techs = [
- "html",
- "css",
- "javascript",
- "firebase",
- "node",
- "boostrap",
- "python",
- "react",
- "electron",
- "mongodb"
+ "HTML",
+ "CSS",
+ "Javascript",
+ "Firebase",
+ "Node",
+ "Boostrap",
+ "Python",
+ "React",
+ "Electron",
+ "Mongodb"
 ]
 
-//Criar o baralho
+let cards = null
 
-createCardsDeck(techs)
+function startGame(){
+  cards = createCardsDeck(techs)
+  shuffleCards(cards)
+  console.log(cards)
+
+  initializeCards(cards)
+}
+
+startGame();
+
 
 function createCardsDeck(tech){
-  let cards = []
+  let cards = [];
 
   for(tech of techs){
   cards.push(createPairCard(tech))
@@ -27,6 +39,7 @@ function createCardsDeck(tech){
   
   return cards.flatMap(pair => pair)
 }
+
 
 function createPairCard(tech){
   
@@ -46,4 +59,62 @@ function createPairCard(tech){
 
 function createRandomID(tech){
   return tech + Number.parseInt(Math.random() * 1000)
+}
+
+function shuffleCards(cards){
+  let currentIndex = cards.length
+  let randomIndex = 0
+  
+  while(currentIndex !== 0){
+
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--;    
+
+    [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+  }
+}
+
+function initializeCards(cards){
+
+  let gameBoard = document.getElementById("gameBoard");
+
+  cards.forEach(card=>{
+
+    let cardElement = document.createElement('div')
+    cardElement.id = card.id
+    cardElement.classList.add(CARD)
+    cardElement.dataset.icon = card.tech
+
+    createCardContent(card, cardElement)
+    
+    cardElement.addEventListener('click', flipCard)
+    gameBoard.appendChild(cardElement)
+
+  })
+}
+
+function flipCard(){
+  this.classList.add("flip")
+}
+
+function createCardContent(card, cardElement){
+   createCardFace(FRONT, card, cardElement)
+   createCardFace(BACK, card, cardElement)
+}
+
+function createCardFace(face, card, element){
+  let cardElementFace = document.createElement('div')
+
+  cardElementFace.classList.add(face)
+
+  if(face === FRONT){
+    let iconElement = document.createElement('img')
+    iconElement.classList.add(ICON)
+    iconElement.src = "./assets/images/icon" + card.tech + ".png"
+    cardElementFace.appendChild(iconElement)
+  }else{
+    cardElementFace.innerHTML = "&lt;/&gt";
+  }
+
+  element.appendChild(cardElementFace)
 }
